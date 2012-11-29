29e37feba04449519602c5575c5bea522525b0ce
JM <mue.navarone@gmail.com>
2012-11-28 22:11:16 -0600

    middleware added.

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-var nib = require('nib');
+var nib = require('nib')
+  , stylus = require('stylus');
 
-module.exports = function () {
+var fountain = exports = module.exports = function () {
   return function(style) {
     style
       .use(nib())
       .include(__dirname);
   };
+};
+
+exports.middleware = function (dirname) {
+  console.log('Fountain middleware.');
+  
+  function compile(str, path) {
+    return stylus(str)
+      .set('filename', path)
+      .use(fountain());
+  }
+  
+  return stylus.middleware({
+    src: dirname,
+    force: true,
+    compile: compile
+  });
 };
